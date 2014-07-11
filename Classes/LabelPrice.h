74b21022390fecb83df90257eec40e15d6e09874
btf <402100842@qq.com>
2014-07-11 14:54:34 +0800

    added MenuItemTower

---
+++ Classes/LabelPrice.h
@@ -0,0 +1,10 @@
+#ifndef __LABEL_PRICE_H__
+#define __LABEL_PRICE_H__
+
+#include "cocos2d.h"
+USING_NS_CC;
+class LabelPrice : public Node{
+//	Sprite* backgroundSprite;
+//	Label* label;
+};
+#endif
